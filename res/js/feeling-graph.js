ebf210fd66a0eb1a7eb04ba44471d2d755a9d501
joobro2015 <joobro2015@gmail.com>
2023-02-18 12:25:04 +0900

    physical

---
+++ res/js/feeling-graph.js
@@ -0,0 +1,13 @@
+
+
+$('.chart1').easyPieChart({
+    barColor: '#f16529',
+    trackColor: '#ccc',
+    scaleColor: '#fff',
+    lineCap: 'butt',
+    lineWidth: 30,
+    size: 200,
+    animate: 1000,
+    onStart: $.noop,
+    onStop: $.noop
+  });
