0fa4780fc2746f73073ea82a4937da6c04d44e91
joobro <joobro2015@gmail.com>
2023-02-27 12:50:01 +0900

    physical

--- res/js/feeling-graph.js
+++ res/js/feeling-graph.js
@@ -1,13 +1,47 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
 
+        
 
-$('.chart1').easyPieChart({
-    barColor: '#f16529',
-    trackColor: '#ccc',
-    scaleColor: '#fff',
-    lineCap: 'butt',
-    lineWidth: 30,
-    size: 200,
-    animate: 1000,
-    onStart: $.noop,
-    onStop: $.noop
-  });
+        labels: ['행복', '들뜸', '감동', '화남', '불안', '실망', '슬픔', '평온', '난감'],
+
+        datasets: [{
+            label: '# emotion',
+            data: [10, 10, 10, 10, 10, 10, 10, 10, 10],
+            backgroundColor: [
+                '#A593E0', //행복
+                '#E0E3DA', //들뜸
+                '#f100e5', //감동
+                '#E53A40', // 화남
+                '#f94e3f', //불안
+                '#181842', //실망
+                '#566270', //슬픔
+                '#D7FFF1', //평온
+                '#FBFFB9' //난감
+            ],
+            borderColor: [
+                '#A593E0', //행복
+                '#E0E3DA', //들뜸
+                '#f100e5', //감동
+                '#E53A40', // 화남
+                '#f94e3f', //불안
+                '#181842', //실망
+                '#566270', //슬픔
+                '#D7FFF1', //평온
+                '#FBFFB9' //난감
+            ],
+            borderWidth: 1,
+
+        }]
+    },
+    options: {
+      responsive : true,
+        plugins: {
+            legend: {
+                position: 'bottom'
+            }
+        }
+      }
+});
